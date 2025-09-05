59195480d5dd11a5fd91cc6b692a3530278fdb67
Vitor Avelar <jvitoravelar1200@gmail.com>
2025-09-05 20:29:09 -0300

    criando repositorio

---
+++ src/main/java/vitoravelar/academy/model/User.java
@@ -0,0 +1,18 @@
+package vitoravelar.academy.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class User {
+
+    private Long id;
+
+    private String cpf;
+
+    private String name;
+
+}
