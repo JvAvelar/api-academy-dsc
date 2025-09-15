35920fb5ad50e822ae3caf073ca46b44b97952cf
Vitor Avelar <jvitoravelar1200@gmail.com>
2025-09-15 12:01:17 -0300

    Criando Config do Swagger

---
+++ src/main/java/vitoravelar/academy/config/SwaggerConfig.java
@@ -0,0 +1,34 @@
+package vitoravelar.academy.config;
+
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import io.swagger.v3.oas.models.OpenAPI;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    OpenAPI openAPI() {
+        final String securitySchemeName = "bearerAuth";
+        return new OpenAPI()
+                .info(info())
+                .addSecurityItem(new SecurityRequirement().addList(securitySchemeName))
+                .components(new io.swagger.v3.oas.models.Components()
+                        .addSecuritySchemes(securitySchemeName,
+                                new SecurityScheme()
+                                        .name(securitySchemeName)
+                                        .type(SecurityScheme.Type.HTTP)
+                                        .scheme("bearer")
+                                        .bearerFormat("JWT")));
+    }
+
+    private Info info() {
+        return new Info()
+                .title("Academy")
+                .description("API para sistema de gerenciamento de academia")
+                .version("v1.0.0");
+    }
+}
