0033a26e9da4b42f0c23fff9d31950bb9c7eec95
vitor.avelar <vitor.avelar>
2025-10-01 14:40:15 -0300

    refatorado models

--- src/main/java/vitoravelar/academy/models/Equipment.java
+++ src/main/java/vitoravelar/academy/models/Equipment.java
@@ -4,11 +4,8 @@ import jakarta.persistence.*;
 import lombok.*;
 import vitoravelar.academy.dto.EquipmentDTO;
 
-@Getter
-@Setter
-@AllArgsConstructor
-@NoArgsConstructor
-@EqualsAndHashCode
+import java.util.Objects;
+
 @Entity
 @Table(name = "tb_equipment")
 public class Equipment {
@@ -27,10 +24,55 @@ public class Equipment {
     @JoinColumn(name = "academy_id")
     private Academy academy;
 
+    public Equipment(){}
+
     public Equipment(EquipmentDTO equipmentDTO ){
-        this.id = equipmentDTO.getId();
         this.name = equipmentDTO.getName();
         this.quantity = equipmentDTO.getQuantity();
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Integer getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(Integer quantity) {
+        this.quantity = quantity;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Academy getAcademy() {
+        return academy;
+    }
+
+    public void setAcademy(Academy academy) {
+        this.academy = academy;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        Equipment equipment = (Equipment) o;
+        return Objects.equals(id, equipment.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(id);
     }
 
     public EquipmentDTO toDTO(){
